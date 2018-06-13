11b461d7579b0e044cd54e81efbf04b6efb08413
vencyv <40185467+vencyv@users.noreply.github.com>
2018-06-12 18:12:29 -0600

    test commit

---
+++ app1/hello.java
@@ -0,0 +1,19 @@
+/* java */
+
+import java.io.*;
+
+public class hello{
+	
+	
+  public static void main( String[] args ) {
+	
+		
+		for (int i=1;i<10;i++)
+		{
+			System.out.println("Hello .."+i);
+			
+			
+		}
+	}
+	
+}
