4cc7a036ed0e4a7035b10d13ddeeb74cebb8e03d
vencyv <vencyv@gmail.com>
2018-06-28 15:27:24 -0500

    test

--- app2/HelloVency.java
+++ app2/HelloVency.java
@@ -1,6 +1,6 @@
 public class HelloVency {
     public static void main( String[] args ) {
-        System.out.println( "Hello Vency" );
+        System.out.println( "Hello Vency!" );
         System.exit( 0 ); //success
     }
 }
